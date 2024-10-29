d04536455e7ad12c72d3cc264b625d5b0520808d
SVNIX Solutions <contact@svnix.solutions>
2024-10-29 10:10:10 +0000

    Added settings

---
+++ document_signer_for_pdf/document_signer_for_pdfs/doctype/document_signer_for_pdfs/document_signer_for_pdfs.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, SVNIX Solutions and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Document Signer for PDFs', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ document_signer_for_pdf/document_signer_for_pdfs/doctype/document_signer_for_pdfs/test_document_signer_for_pdfs.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, SVNIX Solutions and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestDocumentSignerforPDFs(FrappeTestCase):
+	pass
