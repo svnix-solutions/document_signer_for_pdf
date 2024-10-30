778423ef36a481f43641808e1194582d33128854
svnix-solutions <contact@svnix.solutions>
2024-10-30 06:47:16 +0000

    Removed DocType

--- document_signer_for_pdf/document_signer_for_pdfs/doctype/document_signer_for_pdfs/document_signer_for_pdfs.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) 2024, SVNIX Solutions and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-class DocumentSignerforPDFs(Document):
-	pass
