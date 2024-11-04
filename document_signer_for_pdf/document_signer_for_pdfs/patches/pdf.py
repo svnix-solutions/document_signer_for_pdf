fc32d49692950efee1fdd3a9ecf644edb557865f
svnix-solutions <contact@svnix.solutions>
2024-11-04 07:54:10 +0000

    Updated pdf.py

--- document_signer_for_pdf/document_signer_for_pdfs/patches/pdf.py
+++ document_signer_for_pdf/document_signer_for_pdfs/patches/pdf.py
@@ -14,67 +14,67 @@ from pyhanko.pdf_utils.incremental_writer import IncrementalPdfFileWriter
 from frappe.utils import pdf
 
 PDF_CONTENT_ERRORS = [
-	"ContentNotFoundError",
-	"ContentOperationNotPermittedError",
-	"UnknownContentError",
-	"RemoteHostClosedError",
+    "ContentNotFoundError",
+    "ContentOperationNotPermittedError",
+    "UnknownContentError",
+    "RemoteHostClosedError",
 ]
 
 def signed_get_pdf(html, options=None, output: PdfWriter | None = None):
-	html = scrub_urls(html)
-	html, options = pdf.prepare_options(html, options)
+    html = scrub_urls(html)
+    html, options = pdf.prepare_options(html, options)
 
-	options.update({"disable-javascript": "", "disable-local-file-access": ""})
+    options.update({"disable-javascript": "", "disable-local-file-access": ""})
 
-	filedata = ""
-	if LooseVersion(pdf.get_wkhtmltopdf_version()) > LooseVersion("0.12.3"):
-		options.update({"disable-smart-shrinking": ""})
+    filedata = ""
+    if LooseVersion(pdf.get_wkhtmltopdf_version()) > LooseVersion("0.12.3"):
+        options.update({"disable-smart-shrinking": ""})
 
-	try:
-		# Set filename property to false, so no file is actually created
-		filedata = pdfkit.from_string(html, options=options or {}, verbose=True)
+    try:
+        # Set filename property to false, so no file is actually created
+        filedata = pdfkit.from_string(html, options=options or {}, verbose=True)
 
-		# create in-memory binary streams from filedata and create a PdfReader object
-		reader = PdfReader(io.BytesIO(filedata))
-	except OSError as e:
-		if any([error in str(e) for error in PDF_CONTENT_ERRORS]):
-			if not filedata:
-				print(html, options)
-				frappe.throw(_("PDF generation failed because of broken image links"))
+        # create in-memory binary streams from filedata and create a PdfReader object
+        reader = PdfReader(io.BytesIO(filedata))
+    except OSError as e:
+        if any([error in str(e) for error in PDF_CONTENT_ERRORS]):
+            if not filedata:
+                print(html, options)
+                frappe.throw(_("PDF generation failed because of broken image links"))
 
-			# allow pdfs with missing images if file got created
-			if output:
-				output.append_pages_from_reader(reader)
-		else:
-			raise
-	finally:
-		pdf.cleanup(options)
+            # allow pdfs with missing images if file got created
+            if output:
+                output.append_pages_from_reader(reader)
+        else:
+            raise
+    finally:
+        pdf.cleanup(options)
 
-	if "password" in options:
-		password = options["password"]
+    if "password" in options:
+        password = options["password"]
 
-	if output:
-		output.append_pages_from_reader(reader)
-		return output
+    if output:
+        output.append_pages_from_reader(reader)
+        return output
 
-	writer = PdfWriter()
-	writer.append_pages_from_reader(reader)
+    writer = PdfWriter()
+    writer.append_pages_from_reader(reader)
 
-	if "password" in options:
-		writer.encrypt(password)
+    if "password" in options:
+        writer.encrypt(password)
 
-	if frappe.conf.signature_pem_file and frappe.conf.signature_key_file:
-		filedata = sign_pdf(
-			io.BytesIO(filedata), 
-			pem_file=frappe.conf.signature_pem_file, 
-			key_file=frappe.conf.signature_key_file, 
-			ca_chain_files=frappe.conf.signature_ca_chain_files, 
-			options=options
-		)
-	else:
-		filedata = pdf.get_file_data_from_writer(writer)
+    if frappe.conf.signature_pem_file and frappe.conf.signature_key_file:
+        filedata = sign_pdf(
+            io.BytesIO(filedata), 
+            pem_file=frappe.conf.signature_pem_file, 
+            key_file=frappe.conf.signature_key_file, 
+            ca_chain_files=frappe.conf.signature_ca_chain_files, 
+            options=options
+        )
+    else:
+        filedata = pdf.get_file_data_from_writer(writer)
 
-	return filedata
+    return filedata
 
 def sign_pdf(input_pdf_io, pem_file, key_file, ca_chain_files, options=None):
     # Load the certificate and private key
@@ -83,7 +83,7 @@ def sign_pdf(input_pdf_io, pem_file, key_file, ca_chain_files, options=None):
     with open(key_file, 'rb') as k_file:
         key = load_private_key_from_pemder(key_file, passphrase=None)
 
-	# Load CA chain if provided
+    # Load CA chain if provided
     ca_chain = []
     if ca_chain_files:
         try:
@@ -94,8 +94,8 @@ def sign_pdf(input_pdf_io, pem_file, key_file, ca_chain_files, options=None):
                 raise ValueError("No CA chain certificates found in the provided CA chain files")
         except Exception as e:
             raise ValueError(f"Error loading CA chain: {e}")
-	
-	# Create a signer object
+    
+    # Create a signer object
     signer = signers.SimpleSigner(signing_cert=cert, signing_key=key, cert_registry=ca_chain)
 
     # Create an IncrementalPdfFileWriter for the input PDF
