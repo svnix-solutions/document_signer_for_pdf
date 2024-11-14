517878ea35a50cbd97d5392f3c19e3851985a767
SVNIX Solutions <contact@svnix.solutions>
2024-11-14 19:53:49 +0000

    figuring out the problem

--- document_signer_for_pdf/__init__.py
+++ document_signer_for_pdf/__init__.py
@@ -1,4 +1,2 @@
 
 __version__ = '0.0.1'
-
-from document_signer_for_pdf.document_signer_for_pdfs.patches import pdf
---
+++ document_signer_for_pdf/document_signer_for_pdfs/__init__.py
@@ -0,0 +1,1 @@
+from document_signer_for_pdf.document_signer_for_pdfs.patches import pdf
